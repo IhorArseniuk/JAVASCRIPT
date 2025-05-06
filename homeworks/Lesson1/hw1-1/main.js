64779d057f1c204ed8501a93560ee42fb7aeaf59
IhorArseniuk <ihorarsenykk.a@gmail.com>
2025-05-06 18:24:25 +0200

    done homework for the first Lesson

---
+++ homeworks/Lesson1/hw1-1/main.js
@@ -0,0 +1,54 @@
+{
+    let hello = 'hello';
+    let owu = 'owu';
+    let com = 'com';
+    let ua = 'ua';
+    let a = 1;
+    let b = 10;
+    let c = -999;
+    let v = 123;
+    let n = parseFloat(3.14);
+    let m = parseFloat(2.7);
+    let s = 16;
+    let d = true;
+    let f = false;
+    console.log(hello);
+    console.log(owu);
+    console.log(com);
+    console.log(ua);
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(v);
+    console.log(n);
+    console.log(m);
+    console.log(s);
+    console.log(d);
+    console.log(f);
+}
+//task 1-1//
+{
+    let fistName = 'Ihor';
+    let midleName = 'Arseniuk';
+    let lastName = 'Ivanovich';
+    let person = `${fistName} ${midleName} ${lastName}`;
+    console.log(person);
+}
+//task 1-2//
+{
+    let a = 100;
+    let b = '100';
+    let c = true;
+    console.log(typeof a);
+    console.log(typeof b);
+    console.log(typeof c);
+}
+//task 1-3//
+{
+    let name = prompt('What is your name?', 'Ihor');
+    let lastName = prompt('What is your last name?', 'Ivanovich');
+    let age = prompt('How old are you', 19);
+    let person = `${name} ${lastName} ${age}`;
+    console.log(person);
+}
+//task 1-4//
