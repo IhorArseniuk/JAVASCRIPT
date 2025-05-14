b78775dd0cca35b24843c48b1e75132623fa2548
IhorArseniuk <ihorarsenykk.a@gmail.com>
2025-05-14 21:48:57 +0200

    work on homework 3 bonus

---
+++ homeworks/Lesson3/hw3-bonus/main.js
@@ -0,0 +1,61 @@
+// task #WpkK0ZH1 //
+{
+    let a=[ 11, 222, 45,56,67,'delta','alpha','beta','deca','lino',true,false,'figo',33,12]
+    console.log(a);
+}
+// task #4aDbSgh //
+{
+    let b=[0];
+    b[0]= 12;
+    b[1]='gigo';
+    b[2]=true;
+    b[3]=43;
+    b[4]=12;
+    b[5]='link'
+    console.log(b);
+}
+// task #qLQLJSeN7i //
+{
+    let c = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+     let i=0;
+    //1 while(i<c.length){
+    //     console.log(c[i]);
+    //     i++;
+    // }
+    //2 for (let i=0; i<c.length;i++){
+    //     console.log(c[i])
+    // }
+    //3let i = 0;
+    //while (i < c.length) {
+    //    if (i % 2 !== 0){
+    //        console.log(c[i]);
+    //    }
+    //    i++;
+    //}
+    //4for (let i = 0; i < c.length;i++){
+    //    if( i%2 !==0){
+    //        console.log(c[i]);
+    //    }
+    //}
+    //5while(i<c.length){
+    //    if(i%2 ===0){
+    //        console.log(c[i]);
+    //    }
+    //    i++;
+    //}
+   //6 for(let i=0; i<c.length;i++){
+   //     if(i%2===0){
+   //         console.log(c[i]);
+   //     }
+   //     i++;
+   // }
+    //7for(let i=0;i<c.length;i++){
+    //    if(i % 3===0){
+    //        c[i]='okten';
+    //        console.log(i,c[i])
+    //    }
+    //}
+
+    
+
+}
