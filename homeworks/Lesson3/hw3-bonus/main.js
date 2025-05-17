0fc9434920b71182b649b28a0cb982a6e2e3e33f
IhorArseniuk <ihorarsenykk.a@gmail.com>
2025-05-18 01:59:20 +0200

    work on homework 3 bonus almost donemaybe

--- homeworks/Lesson3/hw3-bonus/main.js
+++ homeworks/Lesson3/hw3-bonus/main.js
@@ -293,3 +293,25 @@ let priceMidl=priceSum/priceArray.length;
     }
     console.log(secondMixedArray);
 }
+// task //
+{
+    let usersWithId = [
+        {id: 1, name: 'vasya', age: 31, status: false,},
+        {id: 2, name: 'petya', age: 30, status: true,},
+        {id: 3, name: 'kolya', age: 29, status: true,},
+        {id: 4, name: 'olya', age: 28, status: false,}
+    ];
+
+    let citiesWithId = [
+        {user_id: 3, country: 'USA', city: 'Portland'},
+        {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+        {user_id: 2, country: 'Poland', city: 'Krakow'},
+        {user_id: 4, country: 'USA', city: 'Miami'}
+    ];
+    for(let i=1;i<usersWithId.length;i++){
+        let user=usersWithId[i];
+        user[user.address]=citiesWithId[u].country, citiesWithId[i.id-1].city;
+    }
+    console.log(usersWithId);
+    let usersWitchCities=[]
+}
