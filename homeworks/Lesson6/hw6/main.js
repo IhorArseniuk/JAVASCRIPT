e92c0bcd8c6dd4b1085de6168c6f32fb9cea51f5
IhorArseniuk <ihorarsenykk.a@gmail.com>
2025-05-28 23:12:55 +0200

    done hw6

--- homeworks/Lesson6/hw6/main.js
+++ homeworks/Lesson6/hw6/main.js
@@ -186,8 +186,14 @@ console.log('');
 }
 // task 6-10 #bolvdlhP //
 {
+    console.log('');
     let cardsHolder=[];
-    let cards=(suit,color)=>{
+    let cards=(suit)=>{
+        if(suit==='heart'||suit==='diamond'){
+            color='red'
+        }else if(suit==='spade'||suit==='club'){
+            color='black'
+        }else{ return 'Try again'}
          let suits=   [   {  cardSuit:suit, value: 6, color: color},
                 {  cardSuit:suit, value: 7, color: color},
                 {  cardSuit:suit, value: 8, color: color},
@@ -197,10 +203,30 @@ console.log('');
                 {  cardSuit:suit, value: 'queen', color:color},
                 {  cardSuit:suit, value: 'king', color:color},
                 {  cardSuit:suit, value: 'ace', color: color}]
-            cardsHolder.push(suits)}
-    cards('heart','red');
-    cards('diamond','red');
-    cards('spade','black');
-    cards('club','black');
+            cardsHolder.push(...suits)}
+    cards('heart',);
+    cards('diamond');
+    cards('spade');
+    cards('club');
     console.log(cardsHolder);
+    console.log(cardsHolder.find((item)=>{return item.cardSuit==='spade'&& item.value==='ace';}));
+    console.log(cardsHolder.filter(value=> value.value===6))
+    console.log(cardsHolder.filter(value=> value.color==='red'))
+    console.log(cardsHolder.filter(value=>value.cardSuit==='diamond'))
+    console.log(cardsHolder.filter((value)=>{return value.cardSuit==='club'&& value.value!==6&& value.value!==7&& value.value!==8&& value.value!==9 }))
+
+// task 6-11 #EP5I1UUzAX //
+console.log('');
+const redusedCards=cardsHolder.reduce((accumulator,value)=>{
+    if(value.cardSuit==='heart'){
+        accumulator.heart.push(value);
+    }else if(value.cardSuit==='diamond'){
+        accumulator.diamond.push(value);
+    }else if(value.cardSuit==='spade'){
+        accumulator.spade.push(value);
+    }else if(value.cardSuit==='club'){
+        accumulator.club.push(value);
+    }return accumulator;},
+    {heart:[], diamond:[],spade:[] , club:[],});
+console.log(redusedCards);
 }
