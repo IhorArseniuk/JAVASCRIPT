f3f96d54465e8e4c58e948d2309acfd659bb005f
IhorArseniuk <ihorarsenykk.a@gmail.com>
2025-06-06 22:34:38 +0200

    almost done hw7

--- homeworks/Lesson7/hw7/main.js
+++ homeworks/Lesson7/hw7/main.js
@@ -198,3 +198,23 @@ console.log('')
     console.log('')
     console.log(girls.find(Cinderella=>Cinderella.footSize===38))
 }
+// task 7-9 #gsKLAsNWM //
+
+const users3 =[
+    new User(1 ,'Eduard','Shevchenko','loveUkraine.@gmail.com','+380587624456'),
+    new User(2 ,'Mikola','Lewandoski','loveUkraine.@gmail.com','+380587625456'),
+    new User(3 ,'Elizabeth','Luka','loveUkraine.@gmail.com','+380587626756'),
+    new User(4,'Maria','Chmelnutski','loveUkraine.@gmail.com','+380387128756'),
+    new User(5 ,'Liza','Semigora','loveUkraine.@gmail.com',),
+    new User(6 ,'Kyrylo','Javdoshak','loveUkraine.@gmail.com','+380987128756'),
+    new User(7 ,'Ihor','Nicolovski','loveUkraine.@gmail.com','+3809870943456'),
+]
+Array.prototype.ownfilter=function(callback){
+    const result=[];
+    for(let i=0;i<this.length;i++){
+    if(callback(this[i],i,this)) {result.push(this[i])}
+    }
+    return result;
+    };
+let filtred=users3.ownfilter(function (n) {return n.id%2===0})
+console.log(filtred);
