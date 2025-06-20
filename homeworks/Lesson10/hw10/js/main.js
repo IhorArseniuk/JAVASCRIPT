fd2481aec248fd157ad2995cf9d659fa714f5df5
IhorArseniuk <ihorarsenykk.a@gmail.com>
2025-06-20 22:43:55 +0200

    done hw 10

--- homeworks/Lesson10/hw10/js/main.js
+++ homeworks/Lesson10/hw10/js/main.js
@@ -121,3 +121,76 @@ let formConvertator=document.forms.weightConvert
             tr.appendChild(td)}
     }})
 }
+// task 10-9 #bq1zkx7WP //
+{
+   let hrivna=+localStorage.getItem('hrivna')
+    hrivna+=10
+    function fo(){localStorage.setItem('hrivna',hrivna)}
+    let div=document.getElementById('hrochi');
+    div.innerText=100+hrivna+'грн'
+    setInterval(fo,10000)
+}
+// task 10-10 #NKB0tgWIK1G //
+{
+    function stringCreator (Length,customCharacters){
+        const characters = customCharacters || 'QWERTYUIOPASDFGHJKLZXCVBNM';
+        const charactersLength = characters.length;
+        let result='';
+        for(let i =0;i<Length;i++){
+            const randomNumber= Math.floor(Math.random()*charactersLength);
+            result+= characters.charAt(randomNumber)
+        }
+        return result;
+    }
+    function users  (name,surname,count){
+        const counts=count
+        let result=[]
+        for(let i =0;i<count;i++) {
+            const user={}
+            user.name = stringCreator(name);
+            user.surname = stringCreator(surname);
+         /*   let set =new Set
+            set.add(Math.floor(Math.random()*1000))
+            user.id=set*/
+            user.id=Math.floor(Math.random()*100+1);
+            result.push(user)
+        }return result ;}
+
+    let usersArray=users(6,8,100)
+
+    let divStorage=document.getElementById('storageDiv')
+
+   let butnPrev=document.getElementById('prev');
+    let butnNext=document.getElementById('next');
+
+    let usersPerPage=10;
+    let curentPage=0;
+
+function showPage(page){
+    divStorage.innerHTML = '';
+    const startIndex= page * usersPerPage;
+    const endIndex= startIndex + usersPerPage;
+   const usersOnPage= usersArray.slice(startIndex,endIndex)
+
+    usersOnPage.forEach(user=>{
+        let div=document.createElement('div')
+        div.className='user'
+        div.innerHTML=`<h2>${user.name}</h2><h3>${user.surname}</h3><p>${user.id}</p>`;
+        divStorage.appendChild(div);
+    })
+    butnPrev.disabled = page === 0;
+    butnNext.disabled = endIndex >= usersArray.length;
+}
+    document.addEventListener('DOMContentLoaded',()=>{showPage(curentPage)})
+
+
+    butnNext.addEventListener('click',()=>{
+     curentPage++;
+    showPage(curentPage);});
+
+     butnPrev.addEventListener('click',()=>{
+         curentPage--;
+         showPage(curentPage);
+     })
+    console.log(usersArray)
+}
